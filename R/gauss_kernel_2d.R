92e0751108c805ada231ba9d554499b49d83fd24
Hannah Glaser <hannah.glaser@mk-glaser.de>
2024-03-14 16:24:40 +0100

    fix gauss_fct

--- R/gauss_kernel_2d.R
+++ R/gauss_kernel_2d.R
@@ -1,17 +1,24 @@
-.onLoad <- function(libname, pkgname) {
-  pyfcts <<- py_run_file( file.path( system.file("extdata",package="DevMethyl"), "smooth_fct_final.py" ) )
-}
-
+#' Title2
+#'
+#' @param x
+#' @param t
+#' @param v
+#' @param xmin
+#' @param xmax
+#' @param delta_x
+#' @param tmin
+#' @param tmax
+#' @param delta_t
+#' @param hx
+#' @param ht
+#'
+#' @return
+#' @export
+#'
+#' @examples
 gauss_kernel_2d <- function(x, t, v, xmin, xmax, delta_x, tmin, tmax, delta_t, hx, ht) {
 
-  #has to be right wd
-
-  # package_file does not give detailed path
-  py_run_file(paste(package_file("inst"), "/extdata/smooth_fct_final.py", sep=""))
-
-  py_install("numba")
-  num <- import(numba)
-  py_gaussian_smoothing(x, t, v, xmin, xmax, delta_x, tmin, tmax, delta_t, hx, ht)
+  pyfcts$gaussian_smoothing(x, t, v, xmin, xmax, delta_x, tmin, tmax, delta_t, hx, ht)
 
 }
 
