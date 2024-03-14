c968acbbedcf6be9fb746a82b90fdf887d33bd2f
Simon Anders <sanders@fs.tum.de>
2024-03-14 15:26:22 +0100

    added numba test function

--- R/gauss_kernel_2d.R
+++ R/gauss_kernel_2d.R
@@ -10,3 +10,8 @@ gauss_kernel_2d <- function(x, t, v, xmin, xmax, delta_x, tmin, tmax, delta_t, h
   py_gaussian_smoothing(x, t, v, xmin, xmax, delta_x, tmin, tmax, delta_t, hx, ht)
 
 }
+
+test_numba <- function() {
+  py_run_file( file.path( system.file("extdata",package="DevMethyl"), "smooth_fct_final.py" ) )
+  py$norm_pdf(1)
+}
